ed75d82bf51cadd8bbccc0ff55a0ba2eddc6c23c
Carlos Mestre <carlos.mestre@fit-tecnologia.org.br>
2018-05-23 18:43:14 -0300

    creating services to add products

--- products-ui/src/app/product-list/product-list.component.ts
+++ products-ui/src/app/product-list/product-list.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductServiceService } from './product-service.service';
+
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  providers: [ProductServiceService]
 })
 export class ProductListComponent implements OnInit {
 
-  products: string[] = ['nescau', 'bolacha', 'arroz'];
-  name: string = "test";
+  products: string[];
+  product: string;
 
-  constructor() { }
+  constructor(private service: ProductServiceService) {
+    this.products = this.service.getProducts();
+  }
 
   ngOnInit() {
+  }
+
+  sendProduct() {
+    this.service.setProduct(this.product);
   }
 
   list() { }
--- products-ui/src/app/product-list/product-service.service.ts
+++ products-ui/src/app/product-list/product-service.service.ts
@@ -3,6 +3,16 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProductServiceService {
 
+  products: string[] = [];
+
   constructor() { }
 
+  getProducts(): string[] {
+    return this.products;
+  }
+
+  setProduct(product: string): void {
+    this.products.push(product);
+  }
+  
 }
